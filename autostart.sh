02e269d3e0f00bf2608aa34f6db8ac91c6bfdf45
Hyd3L <hydel@linuxmail.org>
2018-04-17 22:17:44 +0200

    Uploaded files

---
+++ autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+conky -d -c /home/hyd3l/.conky/hyd3l/hyd3l-arch-right 
+conky -d -c /home/hyd3l/.conky/hyd3l/hyd3l-arch-left 
+conky -d -c /home/hyd3l/.conky/hyd3l/hyd3l-arch-timedate
+ 
